b6ebabe9546a096211c77260889e264100660aed
Nick <nick.moi@gmail.com>
2019-09-21 15:08:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
   longFlag
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strArr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
